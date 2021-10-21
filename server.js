59a702191fb7b8f96be2eda9c278293dd67ca201
luanlsr <luanfswd@gmail.com>
2021-10-21 20:12:50 -0300

    feat: :sparkles: implementado socket.io

--- server.js
+++ server.js
@@ -3,3 +3,32 @@ const path = require('path');
 
 const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (_req, res) => {
+  res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', (socket) => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit('previuosMessages', messages);
+
+  socket.on('sendMessage', data => {
+    messages.push(data)
+    console.log(data);
+    socket.broadcast.emit('recivedMessage', data)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Servidor online!');
+  console.log('http://localhost:3000');
+});
